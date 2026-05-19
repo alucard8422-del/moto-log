defa868aa10d98d5a2988970104097b6523a49c3
HONG <alucard8422@gmail.com>
2026-05-19 23:31:36 +0900

    update

--- src/pages/CourseSharePage.tsx
+++ src/pages/CourseSharePage.tsx
@@ -184,7 +184,7 @@ function DiarySheet({ course, onClose, onPublish }: DiarySheetProps) {
         </div>
       </div>
 
-      <input ref={fileRef} type="file" accept=".jpg,.jpeg,.png,.gif,.webp,.heic,.heif" className="hidden" onChange={handleFile} />
+      <input ref={fileRef} type="file" accept="image/jpeg,image/png,image/gif,image/webp,image/heic,image/heif" className="hidden" onChange={handleFile} />
     </>
   )
 }
